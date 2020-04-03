c9ac7c1f78ffc7f38ca32f8803ad0a43242b2147
Nils Emmenegger <61921611+gingers-have-souls@users.noreply.github.com>
2020-04-03 01:03:23 -0400

    Refined the scratchpad windows script by improving efficiency (not that it matters with python), removing a useless bit, and commenting the hell out of it

--- .config/polybar/scripts/modifyscrathpadwindows.py
+++ .config/polybar/scripts/modifyscrathpadwindows.py
@@ -4,19 +4,27 @@ from i3ipc import Connection, Event
 
 i3 = Connection()
 
-workspaces = i3.get_workspaces()
-wsinuse = []
-
-for i in workspaces:
-    wsinuse.append(i.num)
-
 scratch = i3.get_tree().scratchpad().leaves()
 
-if len(scratch) > 0 or 42 in wsinuse:
+# If there is something in the scratchpad
+# Lists are true if not empty
+if scratch:
+    #switch to workspace 42 and making the windows stack
     i3.command("workspace 42")
     i3.command("layout stacking")
     
-    for i in scratch:
-        if len(i.focus) < 1:
-            i.command("scratchpad show")
-            i.command("floating toggle")
+    #remove each window from scratchpad and defloat it
+    for window in scratch:
+        window.command("scratchpad show")
+        window.command("floating toggle")
+else:
+    #make list of used workspaces
+    workspaces = i3.get_workspaces()
+    wsinuse = []
+
+    for i in workspaces:
+        wsinuse.append(i.num)
+    
+    #if workspace 42 is already in use, switch to it
+    if 42 in wsinuse:
+        i3.command("workspace 42")
