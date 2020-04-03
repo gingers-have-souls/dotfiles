f3a0b2f6cf03955105f431663e92642e84a5ed12
Nils Emmenegger <61921611+gingers-have-souls@users.noreply.github.com>
2020-04-03 00:28:35 -0400

    1. Changed the show all scratchpad thing so it switches to it not only if there's something in the scratchpad but also if there's already something there 2. Changed Moyai description in emojisearch to be more descriptive

--- .config/polybar/scripts/modifyscrathpadwindows.py
+++ .config/polybar/scripts/modifyscrathpadwindows.py
@@ -4,8 +4,15 @@ from i3ipc import Connection, Event
 
 i3 = Connection()
 
+workspaces = i3.get_workspaces()
+wsinuse = []
+
+for i in workspaces:
+    wsinuse.append(i.num)
+
 scratch = i3.get_tree().scratchpad().leaves()
-if len(scratch) > 0:
+
+if len(scratch) > 0 or 42 in wsinuse:
     i3.command("workspace 42")
     i3.command("layout stacking")
     
