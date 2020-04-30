7d76ab10cbd4468f4759f7dd9499727d77dd60e7
Nils Emmenegger <61921611+gingers-have-souls@users.noreply.github.com>
2020-04-30 18:25:48 -0400

    Renamed emoji.rasi to thicc.rasi and started using it for dunstrc menus

--- .config/rofi/scripts/emojisearch.sh
+++ .config/rofi/scripts/emojisearch.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-(sed 's/\t/ | /g' $HOME/.config/rofi/scripts/emoji-data/data/all_emojis.txt; cat $HOME/.config/rofi/scripts/extrasymbols) | rofi -dmenu -p "" -i -theme emoji | awk '{print $1}' | tr -d '\n' | xclip -selection clipboard && xdotool key "ctrl+shift+v"
+(sed 's/\t/ | /g' $HOME/.config/rofi/scripts/emoji-data/data/all_emojis.txt; cat $HOME/.config/rofi/scripts/extrasymbols) | rofi -dmenu -p "" -i -theme thicc | awk '{print $1}' | tr -d '\n' | xclip -selection clipboard && xdotool key "ctrl+shift+v"
